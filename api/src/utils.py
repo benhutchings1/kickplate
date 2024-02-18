5f2faf617afb5b41dff4e5c6390202a7178eabb8
ben hutchings <ben.hutchings46@gmail.com>
2024-02-18 16:53:18 +0000

    Schema validation method

---
+++ api/src/utils.py
@@ -0,0 +1,17 @@
+import json
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
+def validate_against_schema(json_schema:dict, json_def:str) -> tuple[bool, str]:
+    try:
+        validate(instance=json_def, schema=json_schema)
+        return (True, True) # Double up to preserve tuple consistency
+    except ValidationError as e:
+        return (False, e)
+    except e:
+        return (False, "Schema validation error")
+
+
+def read_schema(path:str) -> dict:
+    with open(path, "r") as fs:
+        return json.loads(fs.read())

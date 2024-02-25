5c5e18a446c55f27e4e86c96b68091f65b150102
ben hutchings <ben.hutchings46@gmail.com>
2024-02-25 17:12:59 +0000

    Enhanced error handling

--- api/src/create_exec_graph.py
+++ api/src/create_exec_graph.py
@@ -1,50 +1,80 @@
 from kubernetes.client.exceptions import ApiException
+from kubernetes.dynamic.exceptions import ConflictError
+from src.error_handling import CustomError, HttpCodes
 import json
 from src import utils
 import re
 
 def create_exec_graph(json_def:dict, api_version:str="test.deploymentengine.com/v1",
-                       namespace:str="default") -> tuple[bool, str]:
+                       namespace:str="default") -> None:
     # Read in schema
-    with open("src/exec_graph.schema.json", "r") as fs:
-        schema = json.load(fs)
-    
-    # Check validity from schema
-    valid = utils.validate_against_schema(json_schema=schema,
+    schema = utils.read_schema("src/exec_graph.schema.json")
+
+    # Check validity from schema, will raise error if invalid
+    utils.validate_against_schema(json_schema=schema,
                                           json_def=json_def)
-    if not valid[0]:
-        return valid
 
-    # Check graph name is valid
-    regex_engine = re.compile('[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*')
-    if not bool(regex_engine.match(json_def["graphname"])):
-        return (False, "graph name must consist of lower case alphanumeric characters, '-', or '.', and must start/end with an alphanumeric character")
-
+    # Check graph name is valid, will raise error if invalid
+    validate_name(json_def["graphname"])
+    
     # Add meta data to make valid execgraph request
     exec_graph_definition = format_request(json_def, api_version=api_version)
     
-    # Create deployment
-    dy_client = utils.connect_to_kubenetes()
-    resource = dy_client.resources.get(api_version='test.deploymentengine.com/v1', kind='ExecutionGraph')
-    try:
-        # Attempt to create graph resource
-        resource.create(body=exec_graph_definition, namespace=namespace)
-        return (True, True)
-    except ApiException as e:
-        # Format error response
-        error = json.loads(e.body)
-        return (False, f"Code: {error['code']}, Message: {error['message']}")
+    # Submit execgraph
+    submit_execgraph(
+        exec_graph=exec_graph_definition,
+        namespace=namespace,
+        api_version=api_version
+    )
         
 
 def format_request(json_def:dict, api_version:str) -> dict:
-    # Add metadata required for creating ExecutionGraph resource
-    return {
-        "apiVersion": api_version,
-        "kind": "ExecutionGraph",
-        "metadata": {
-            "name": json_def["graphname"]
-        },
-        "spec": {
-            "steps": json_def["steps"]
-        } 
-    }
+    try:
+        # Structure ExecutionGraph resource request
+        return {
+            "apiVersion": api_version,
+            "kind": "ExecutionGraph",
+            "metadata": {
+                "name": json_def["graphname"]
+            },
+            "spec": {
+                "steps": json_def["steps"]
+            } 
+        }
+    except KeyError as e:
+        raise CustomError(
+            message=f"Data {e} missing from graph definition",
+            error_code=HttpCodes.USER_ERROR
+            ) from None
+
+
+def validate_name(execgraph_name:str) -> None:
+    # Check graph name against regex
+    regex_engine = re.compile('[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*')
+    if not bool(regex_engine.match(execgraph_name)):
+        raise CustomError(
+            message=f"graph name [{execgraph_name}] \
+                must consist of lower case alphanumeric characters, '-', or '.', and must start/end with an alphanumeric character",
+            error_code=HttpCodes.USER_ERROR
+        ) from None
+
+
+def submit_execgraph(exec_graph:dict, namespace:str,
+                     api_version:str, kind:str='ExecutionGraph') -> None:
+    # Get resource
+    resource = utils.get_execgraph_resource(api_version=api_version, kind=kind)    
+    
+    # Try submit execgraph
+    try:
+        # Attempt to create graph resource
+        resource.create(body=exec_graph, namespace=namespace)
+    except ApiException as e:
+        # Check for specific known errors        
+        if json.loads(e.body)["code"] == 409:
+            raise CustomError(
+                message=f"{exec_graph['metadata']['name']} already exists",
+                error_code=HttpCodes.CONFLICT
+            ) from None 
+        else:
+            # Unknown error, let top level error handler capture it
+            raise e

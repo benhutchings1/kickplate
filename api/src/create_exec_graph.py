db5913ffe316a40cc99527ab91a232e5ed9da397
ben hutchings <ben.hutchings46@gmail.com>
2024-03-04 22:31:04 +0000

    Modified to use new config options

--- api/src/create_exec_graph.py
+++ api/src/create_exec_graph.py
@@ -1,39 +1,41 @@
 from kubernetes.client.exceptions import ApiException
-from kubernetes.dynamic.exceptions import ConflictError
 from src.error_handling import CustomError, HttpCodes
+from src.config import config
 import json
 from src import utils
 import re
 
-def create_exec_graph(json_def:dict, api_version:str="test.deploymentengine.com/v1",
-                       namespace:str="default") -> None:
+def create_exec_graph(json_def:dict) -> None:
     # Read in schema
-    schema = utils.read_schema("src/exec_graph.schema.json")
+    schema = utils.read_schema("config/exec_graph.schema.json")
 
     # Check validity from schema, will raise error if invalid
     utils.validate_against_schema(json_schema=schema,
-                                          json_def=json_def)
+                                    json_def=json_def)
 
     # Check graph name is valid, will raise error if invalid
     validate_name(json_def["graphname"])
     
     # Add meta data to make valid execgraph request
-    exec_graph_definition = format_request(json_def, api_version=api_version)
+    exec_graph_definition = format_request(
+        json_def=json_def, 
+        api_version=config.get()
+    )
     
     # Submit execgraph
     submit_execgraph(
         exec_graph=exec_graph_definition,
-        namespace=namespace,
-        api_version=api_version
+        namespace=config.get(section="kube_config", key="namespace"),
+        api_version=config.get(section="exegraph", key="api_version")
     )
         
 
-def format_request(json_def:dict, api_version:str) -> dict:
+def format_request(json_def:dict) -> dict:
     try:
         # Structure ExecutionGraph resource request
         return {
-            "apiVersion": api_version,
-            "kind": "ExecutionGraph",
+            "apiVersion": config.get("execgraph", "api_version"),
+            "kind": config.get("execgraph", "kind"),
             "metadata": {
                 "name": json_def["graphname"]
             },
@@ -59,22 +61,22 @@ def validate_name(execgraph_name:str) -> None:
         ) from None
 
 
-def submit_execgraph(exec_graph:dict, namespace:str,
-                     api_version:str, kind:str='ExecutionGraph') -> None:
+def submit_execgraph(exec_graph:dict) -> None:
     # Get resource
-    resource = utils.get_execgraph_resource(api_version=api_version, kind=kind)    
+    resource = utils.get_resource(api_version=config.get("execgraph", "api_version"),
+                                  kind=config.get("execgraph", "kind")) 
     
     # Try submit execgraph
     try:
         # Attempt to create graph resource
-        resource.create(body=exec_graph, namespace=namespace)
+        resource.create(body=exec_graph, namespace=config.get("kube_config", "namespace"))
     except ApiException as e:
         # Check for specific known errors        
-        if json.loads(e.body)["code"] == 409:
+        if str(json.loads(e.body)["code"]) == str(409):
             raise CustomError(
                 message=f"{exec_graph['metadata']['name']} already exists",
                 error_code=HttpCodes.CONFLICT
-            ) from None 
+            ) from e 
         else:
             # Unknown error, let top level error handler capture it
             raise e

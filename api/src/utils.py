0cfc2a050587759651d8d651190f86fb99d1ea75
ben hutchings <ben.hutchings46@gmail.com>
2024-03-04 22:32:12 +0000

    Use new config options

--- api/src/utils.py
+++ api/src/utils.py
@@ -1,20 +1,12 @@
-"""Common utility functions, accessible over the whole project"""
-import json
 from kubernetes import client, config
-from kubernetes.dynamic import DynamicClient, ResourceInstance
+from kubernetes.dynamic import DynamicClient
+import json
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 from src.error_handling import CustomError, HttpCodes
 
 
-def validate_against_schema(json_schema:dict, json_def:str) -> None:
-    """
-    Validates user defined graph against schema for input validation. 
-    Raises an error if exec graph is invalid, otherwise returns nothing 
-    Inputs:
-        - json_schema (dict) JSON schema
-        - json_def (dict) users JSON definition of exec graph
-    """
+def validate_against_schema(json_schema:dict, json_def:str) -> tuple[bool, str]:
     try:
         # Check validity of user input against schema
         validate(instance=json_def, schema=json_schema)
@@ -27,65 +19,44 @@ def validate_against_schema(json_schema:dict, json_def:str) -> None:
 
 
 def read_schema(path:str) -> dict:
-    """
-    Read JSON schema for execution graph definitions
-    Inputs:
-        - path (str) path to .json file exec graph schema
-    
-    Outputs:
-        - (dict) contents of schema file
-    """
     try:
-        with open(path, "r", encoding="utf-8") as fs:
+        with open(path, "r") as fs:
             return json.loads(fs.read())
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         raise CustomError(
             error_code=HttpCodes.INTERNAL_SERVER_ERROR,
             logging_message=f"File {path} not found"
-        ) from e
-    except IOError as e:
+        ) 
+    except IOError:
         raise CustomError(
             error_code=HttpCodes.INTERNAL_SERVER_ERROR,
-            logging_message=f"IOError, error message: {e}"
-        ) from e
+            logging_message=f""
+        ) 
 
 
 def connect_to_kubenetes() -> DynamicClient:
-    """
-    Connects to kubernetes cluster to perform actions. Uses predefined kubernetes config
-    (default found in ~/.kube) to authorise and connect
-    
-    Outputs:
-        - (DynamicClient) client object connected to the cluster
-    """
     try:
         config.load_kube_config()
         return DynamicClient(client.ApiClient())
     except Exception as e:
         raise CustomError(
             error_code=HttpCodes.INTERNAL_SERVER_ERROR,
-            logging_message=f"Error connecting to k8, error message {e}"
-        ) from e
+            logging_message=f"Error connecting to k8, error message {e.message}"
+        )
 
-
-def get_execgraph_resource(api_version, kind) -> ResourceInstance:
-    """
-    Uses DynamicClient to retrieve the ExecutionGraph resource
-    input:
-        - api_version (str) version of API to interrogate
-        - kind (str) singular resource name
-    
-    Output:
-        - (ResourceInstance) resource to fill 
-    """
+def get_resource(api_version, kind) -> DynamicClient:
     # Get dynamic client
     dy_client = connect_to_kubenetes()
+    
     # Try get ExecutionGraph resource
     try:
         return dy_client.resources.get(api_version=api_version, kind=kind)
     except Exception as e:
         raise CustomError(
             error_code=HttpCodes.INTERNAL_SERVER_ERROR,
-            logging_message=str(e)
-        ) from e
+            logging_message=e.message
+        )
         
+def get_co_client_api() -> client.CustomObjectsApi:
+    connect_to_kubenetes()
+    return client.CustomObjectsApi()

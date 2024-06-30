5d72d0bf338742792becb89c10e698f1599b508c
ben hutchings <ben.hutchings46@gmail.com>
2024-06-30 21:31:28 +0100

    move files

--- modules/acr/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_container_registry" "de_acr" {
-    name                = "deployment_engine_${var.env}_acr"
-    resource_group_name = var.resource_group_name
-    location            = var.location
-    sku                 = var.sku
-    tags                = var.tags
-}

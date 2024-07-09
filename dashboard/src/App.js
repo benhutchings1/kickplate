b72bf3e24f6206f6521fac763cfd7aba60029316
ben hutchings <ben.hutchings46@gmail.com>
2024-07-09 20:05:44 +0100

    feat: initial layout

---
+++ dashboard/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// Bootstrap Bundles
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React! this</h1>
+      </div>
+    )
+  }
+}
+
+export default App

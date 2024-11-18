65d5e0b7c61ea40105cb5cdb396249682551d804
Ben Hutchings <ben.hutchings@gmail.com>
2024-11-18 19:37:22 +0000

    fix: remove error handling

--- k8s/operators/graphmanager/internal/controller/edag_config.go
+++ k8s/operators/graphmanager/internal/controller/edag_config.go
@@ -7,11 +7,11 @@ type Config struct {
 	DefaultPodLabels map[string]string
 }
 
-func LoadConfig() (config Config, err error) {
+func LoadConfig() (config Config) {
 	return Config{
 		Namespace:        "default",
 		FinaliserPath:    "graph.kickplate.com/finalizer",
 		DefaultPortpod:   8000,
 		DefaultPodLabels: map[string]string{},
-	}, nil
+	}
 }

8b8683c9bfaf305f1bf2054e2cd314546a9b9c80
Ben Hutchings <ben.hutchings@gmail.com>
2024-12-08 13:32:45 +0000

    settings

--- api/settings.py
+++ api/settings.py
@@ -1,16 +1,22 @@
-from pydantic_settings import BaseSettings
-from typing import Optional
+import os
+
+from pydantic import BaseModel
 
 
-class _Settings(BaseSettings):
-    DEBUG_MODE: bool = True
-    CLUSTER_HOST: Optional[str] = None
-    CLUSTER_SERVICE_ACCOUNT_SECRET: Optional[str] = None
-    CLUSTER_CERTIFICATE_PATH: Optional[str] = None
+class Settings(BaseModel):
 
-    AUTH_JWKS_URL: Optional[str] = None
-    AUTH_ISSUER: Optional[str] = None
-    AUTH_AUDIENCE: Optional[str] = None
+    DEBUG_MODE: bool = os.environ["DEBUG_MODE"] == "true"
+    CLUSTER_HOST: str = os.environ["CLUSTER_HOST"]
+    CLUSTER_SERVICE_ACCOUNT_SECRET: str = os.environ["CLUSTER_SERVICE_ACCOUNT_SECRET"]
+    CLUSTER_CERTIFICATE_PATH: str = os.environ["CLUSTER_CERTIFICATE_PATH"]
+
+    AUTH_TOKEN_URL: str = os.environ["AUTH_TOKEN_URL"]
+    AUTH_AUTH_URL: str = os.environ["AUTH_AUTH_URL"]
+    AUTH_OIDC_CONFIG_URL: str = os.environ["AUTH_OIDC_CONFIG_URL"]
+    AUTH_ISSUER: str = os.environ["AUTH_ISSUER"]
+    AUTH_AUDIENCE: str = os.environ["AUTH_AUDIENCE"]
+    AUTH_REQUIRED_ROLES: list[str] = os.environ["AUTH_REQUIRED_ROLES"].split(",")
+    AUTH_CLIENT_ID: str = os.environ["AUTH_CLIENT_ID"]
 
 
-settings = _Settings()
+settings = Settings()

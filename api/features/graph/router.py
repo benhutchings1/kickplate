c75b2cc3dab863d3d95b79c8bf32d04080cac5ca
Ben Hutchings <ben.hutchings@gmail.com>
2024-12-10 20:10:30 +0000

    feat: skeleton endpoints

--- api/features/graph/router.py
+++ api/features/graph/router.py
@@ -1,38 +1,83 @@
-from fastapi import APIRouter, Path, Depends, Body
 from typing import Annotated
-from .models import EDAG, RunGraphDetails, RunGraphParameters, GraphStatusDetails
-from .services import GraphServices
-_GRAPH_TAG = "EDAGs"
+
+from fastapi import APIRouter, Body, Depends, Header, Path, Request, Security
+from fastapi.security import OAuth2AuthorizationCodeBearer, SecurityScopes
+
+from auth.models import User, Scopes
+from auth.security import RBACSecurity
+from settings import settings
+
+from .models import EDAGRequest, GraphStatusDetails, RunGraphDetails, RunGraphParameters
+from .services import EDAGServices
 
 router = APIRouter(
     prefix="/api/v1/graph",
-    tags=[_GRAPH_TAG],
 )
 
+_EDAG_TAG = "EDAG"
+_EDAG_EXECUTION_TAG = "EDAG Execution"
 
-@router.post("/create/{graph_name}")
+
+@router.post(
+    "/{edag_name}",
+    tags=[_EDAG_TAG],
+    description="Create new EDAG",
+)
 async def create_edag(
-    graph_name: Annotated[str, Path()],
-    graph: Annotated[EDAG, Body()],
-    graph_services: Annotated[GraphServices, Depends()]
-) -> EDAG:
-    return await graph_services.create_edag(
-        graph_name, graph
-    )
+    edag_name: Annotated[str, Path()],
+    graph: Annotated[EDAGRequest, Body()],
+    graph_services: Annotated[EDAGServices, Depends()],
+) -> EDAGRequest:
+    return await graph_services.create_edag(edag_name, graph)
 
 
-@router.post("/run/{run_id}")
+@router.put("/{edag_name}", tags=[_EDAG_TAG], description="Update an existing EDAG")
+async def update_edag(
+    edag_name: Annotated[str, Path()],
+    updated_graph: Annotated[EDAGRequest, Body()],
+    graph_services: Annotated[EDAGServices, Depends()],
+) -> EDAGRequest:
+    raise NotImplementedError()
+
+
+@router.get("/{edag_name}", tags=[_EDAG_TAG], description="Fetch an existing EDAG")
+async def get_edag(
+    edag_name: Annotated[str, Path()],
+    graph_services: Annotated[EDAGServices, Depends()],
+) -> EDAGRequest:
+    raise NotImplementedError()
+
+
+@router.delete("/{edag_name}", tags=[_EDAG_TAG], description="Delete an existing EDAG")
+async def delete_edag(
+    edag_name: Annotated[str, Path()],
+    graph_services: Annotated[EDAGServices, Depends()],
+) -> EDAGRequest:
+    raise NotImplementedError()
+
+
+@router.post("/{run_id}/run", tags=[_EDAG_EXECUTION_TAG], description="Execute an EDAG")
 async def run_edag(
     run_id: Annotated[str, Path()],
     run_parameters: Annotated[RunGraphParameters, Body()],
-    graph_services: Annotated[GraphServices, Depends()]
+    graph_services: Annotated[EDAGServices, Depends()],
 ) -> RunGraphDetails:
     return await graph_services.run_edag(run_id, run_parameters)
 
 
-@router.get("/run/{run_id}")
+oauth2_scheme = OAuth2AuthorizationCodeBearer(
+    authorizationUrl=settings.AUTH_AUTH_URL,
+    tokenUrl=settings.AUTH_TOKEN_URL,
+)
+
+
+@router.get(
+    "/{run_id}/run",
+    tags=[_EDAG_EXECUTION_TAG],
+    description="Get the status of an existing EDAG run",
+)
 async def get_edag_status(
     run_id: Annotated[str, Path()],
-    graph_services: Annotated[GraphServices, Depends()]
+    graph_services: Annotated[EDAGServices, Depends()],
 ) -> GraphStatusDetails:
     return await graph_services.get_edag_status(run_id)

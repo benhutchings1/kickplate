895f5700941f68bdfccf4805695236d3f9ce8c17
Ben Hutchings <ben.hutchings@gmail.com>
2024-12-10 20:09:04 +0000

    feat: fetch OIDC config on load

--- api/app.py
+++ api/app.py
@@ -1,12 +1,43 @@
+import logging
+from contextlib import asynccontextmanager
+from typing import Any
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.security import OAuth2AuthorizationCodeBearer
 
+from auth.validator import initialise_token_validator
 from error_handling import add_error_handlers
-from features.health.router import router as health_router
 from features.graph.router import router as graph_router
+from features.health.router import router as health_router
 from settings import settings
 
-app = FastAPI()
+logger = logging.Logger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> Any:
+    logger.info("Fetching OIDC config...")
+    initialise_token_validator()
+    logger.info("...Fetched OIDC config")
+
+    yield
+
+
+oauth2_scheme = OAuth2AuthorizationCodeBearer(
+    authorizationUrl=settings.AUTH_AUTH_URL,
+    tokenUrl=settings.AUTH_TOKEN_URL,
+)
+
+app = FastAPI(
+    lifespan=lifespan,
+    swagger_ui_init_oauth={
+        "clientId": settings.AUTH_CLIENT_ID,
+        "scopes": "",
+        "appName": "Kickplate API",
+        "userPkceWithAuthorizationCodeGrant": True,
+    },
+)
 
 add_error_handlers(app)
 app.include_router(health_router)
